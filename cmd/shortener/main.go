85869358fdaf7a80326b097e24c24a67596b791a
s.chotchaeva <s.chotchaeva@vk.team>
2026-05-07 14:31:53 +0300

    iter1: add endpoints
    - add endpoint to create short url
    - add endpoint to get full url

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -15,48 +15,53 @@ func generateID() string {
 	return hex.EncodeToString(b)
 }
 
+func shortenHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "", http.StatusBadRequest)
+		return
+	}
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil || len(body) == 0 {
+		http.Error(w, "", http.StatusBadRequest)
+		return
+	}
+
+	id := generateID()
+	urlStore[id] = string(body)
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("http://localhost:8080/" + id))
+}
+
+func redirectHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Path[1:]
+	if id == "" {
+		http.Error(w, "", http.StatusBadRequest)
+		return
+	}
+
+	originalURL, ok := urlStore[id]
+	if !ok {
+		http.Error(w, "", http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Location", originalURL)
+	w.WriteHeader(http.StatusTemporaryRedirect)
+}
+
+func run() error {
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/`, shortenHandler)
+	mux.HandleFunc(`/{id}`, redirectHandler)
+
+	return http.ListenAndServe(":8080", mux)
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodPost:
-			if r.URL.Path != "/" {
-				http.Error(w, "", http.StatusBadRequest)
-				return
-			}
-			body, err := io.ReadAll(r.Body)
-			if err != nil || len(body) == 0 {
-				http.Error(w, "", http.StatusBadRequest)
-				return
-			}
-			defer r.Body.Close()
-
-			id := generateID()
-			urlStore[id] = string(body)
-
-			w.Header().Set("Content-Type", "text/plain")
-			w.WriteHeader(http.StatusCreated)
-			w.Write([]byte("http://localhost:8080/" + id))
-
-		case http.MethodGet:
-			id := r.URL.Path[1:]
-			if id == "" {
-				http.Error(w, "", http.StatusBadRequest)
-				return
-			}
-			originalURL, ok := urlStore[id]
-			if !ok {
-				http.Error(w, "", http.StatusBadRequest)
-				return
-			}
-			w.Header().Set("Location", originalURL)
-			w.WriteHeader(http.StatusTemporaryRedirect)
-
-		default:
-			http.Error(w, "", http.StatusBadRequest)
-		}
-	})
-
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := run(); err != nil {
 		panic(err)
 	}
 }
